9e3044ab8025458d70ebae3858ad25134e1a6368
belen yammal <belentammal@gmail.com>
2021-02-17 16:13:33 -0300

    modificaciones en js y boton categorias

--- js/productos.js
+++ js/productos.js
@@ -1,30 +1,50 @@
-const pizzas = [
-    { nombre: "muzzarela", precio: 500, imagen: "../imagenes/categorias/gohan.png"},
-    { nombre: "especial", precio: 300, imagen: "../imagenes/categorias/gohan.png"},
-    { nombre: "cuatro quesos", precio: 400, imagen: "../imagenes/categorias/gohan.png"},
-    { nombre: "napolitana", precio: 350, imagen: "../imagenes/categorias/gohan.png"},
-  ]
+const productos = [
+  { id: 1, categoria: "pizzas", nombre: "muzzarela", precio: 500, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 2, categoria: "pizzas", nombre: "especial", precio: 300, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 3, categoria: "pizzas", nombre: "cuatro quesos", precio: 400, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 4, categoria: "pizzas", nombre: "napolitana", precio: 350, imagen: "../imagenes/categorias/gohan.png"},
 
-const ensaladas = [
-    { nombre: "cesar", precio: 500, imagen: "../imagenes/categorias/gohan.png"},
-    { nombre: "mixta", precio: 300, imagen: "../imagenes/categorias/gohan.png"},
-    { nombre: "rusa", precio: 400, imagen: "../imagenes/categorias/gohan.png"},
-    { nombre: "variada", precio: 350, imagen: "../imagenes/categorias/gohan.png"},
-  ]
-  
-  
+  { id: 5, categoria: "ensaladas", nombre: "cesar", precio: 400, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 6, categoria: "ensaladas", nombre: "mixta", precio: 300, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 7, categoria: "ensaladas", nombre: "rusa", precio: 400, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 8, categoria: "ensaladas", nombre: "gohan", precio: 550, imagen: "../imagenes/categorias/gohan.png"},
 
+  { id: 9, categoria: "empanadas", nombre: "jamon y queso", precio: 65, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 10, categoria: "empanadas", nombre: "criolla", precio: 65, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 11, categoria: "empanadas", nombre: "arabe", precio: 70, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 12, categoria: "empanadas", nombre: "matambre", precio: 90, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 13, categoria: "empanadas", nombre: "cebolla", precio: 90, imagen: "../imagenes/categorias/gohan.png"},
+
+  { id: 14, categoria: "bebidas", nombre: "coca cola 500 ml", precio: 100, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 15, categoria: "bebidas", nombre: "sprite 500 ml", precio: 100, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 16, categoria: "bebidas", nombre: "agua saborizada 500 ml", precio: 70, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 17, categoria: "bebidas", nombre: "cerveza andes 500 ml", precio: 120, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 18, categoria: "bebidas", nombre: "cerveza patagonia 500 ml", precio: 130, imagen: "../imagenes/categorias/gohan.png"},
+
+  { id: 19, categoria: "promociones", nombre: "muzza x2 + coca x4 500 ml", precio: 1000, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 20, categoria: "promociones", nombre: "criolla o jyq x 12", precio: 700, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 21, categoria: "promociones", nombre: "andes 500 ml x6", precio: 600, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 22, categoria: "promociones", nombre: "gohan x2", precio: 990, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 23, categoria: "promociones", nombre: "empanadas arabes x12", precio: 750, imagen: "../imagenes/categorias/gohan.png"},
+
+  { id: 24, categoria: "especiales", nombre: "lomito completo", precio: 600, imagen: "../imagenes/categorias/gohan.png"},
+  { id: 25, categoria: "especiales", nombre: "papas con cheedar", precio: 300, imagen: "../imagenes/categorias/gohan.png"},
+]
+
+
+  
 let categoria =  localStorage.getItem('categoria')
-let productos = document.getElementById("productos")
+let productosDiv = document.getElementById("productos")
 let titulo = document.getElementById("encabezado__title")
 
 titulo.textContent = categoria
 
 const fragment = document.createDocumentFragment()
 
-//quiero hacer de alguna forma q dependiendo cual sea la categoria recorra ese array
-//pero no se como, pense en hacer un if por categoria y q dependiendo el nombre de la categoria recorra cada array pero me parece q se necesita escribir mucho codigo inecesariamente
-for ( const objeto of pizzas){ 
+console.log(categoria)
+let filtrado = productos.filter( producto => producto.categoria == categoria)
+console.log(filtrado)
+for ( const objeto of filtrado){ 
 
     let producto = document.createElement("div")
     producto.classList.add("producto")
@@ -45,6 +65,6 @@ for ( const objeto of pizzas){
 }
 
 
-productos.appendChild(fragment)
+productosDiv.appendChild(fragment)
 
 
